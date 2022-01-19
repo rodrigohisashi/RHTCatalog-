93a0ac279b6f909249e44d539c3706f87bf09f9a
Rodrigo Hisashi <rodrigohisa@gmail.com>
2022-01-19 13:20:19 -0300

    Projeto criado

---
+++ backend/src/main/java/com/rhtinterprise/RHTcatalog/RhTcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.rhtinterprise.RHTcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhTcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhTcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rhtinterprise/RHTcatalog/RhTcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhtinterprise.RHTcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhTcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
