872513be77fb23f3607586231df9659bf79604f8
Rodrigo Hisashi <rodrigohisa@gmail.com>
2022-01-22 11:48:31 -0300

    database SEEDING

--- backend/src/main/java/com/rhtinterprise/RHTcatalog/services/CategoryService.java
+++ backend/src/main/java/com/rhtinterprise/RHTcatalog/services/CategoryService.java
@@ -1,10 +1,10 @@
 package com.rhtinterprise.RHTcatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rhtinterprise.RHTcatalog.entities.Category;
 import com.rhtinterprise.RHTcatalog.repositories.CategoryRepository;
@@ -15,7 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 		
