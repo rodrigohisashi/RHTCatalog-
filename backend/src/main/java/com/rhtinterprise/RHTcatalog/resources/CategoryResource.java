1bb094b9440efe5d19e4c206e896864f45551f1b
Rodrigo Hisashi <rodrigohisa@gmail.com>
2022-01-23 17:37:34 -0300

    DTO adicionado

--- backend/src/main/java/com/rhtinterprise/RHTcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/rhtinterprise/RHTcatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rhtinterprise.RHTcatalog.entities.Category;
+import com.rhtinterprise.RHTcatalog.dto.CategoryDTO;
 import com.rhtinterprise.RHTcatalog.services.CategoryService;
 
 @RestController
@@ -19,8 +19,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/rhtinterprise/RHTcatalog/services/CategoryService.java
+++ backend/src/main/java/com/rhtinterprise/RHTcatalog/services/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.rhtinterprise.RHTcatalog.dto.CategoryDTO;
 import com.rhtinterprise.RHTcatalog.entities.Category;
 import com.rhtinterprise.RHTcatalog.repositories.CategoryRepository;
 
@@ -16,8 +17,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll(); 
+		
+		return list.stream().map(x -> new CategoryDTO(x)).toList();
+		
 		
 	}
 
