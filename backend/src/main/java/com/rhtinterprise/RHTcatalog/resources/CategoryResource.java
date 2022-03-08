71e054775baf7319f750a999d189743c9525fc5e
rht123 <rodrigohisa@gmail.com>
2022-03-08 09:12:56 -0300

    Refatorando com Pageable

--- backend/src/main/java/com/rhtinterprise/RHTcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/rhtinterprise/RHTcatalog/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,15 +23,8 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<Page<CategoryDTO>> findAll(
-		@RequestParam(value="page", defaultValue = "0") Integer page,
-		@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-		@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-		@RequestParam(value = "direction", defaultValue = "ASC") String direction
-	) {
-		PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-
-		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 	
